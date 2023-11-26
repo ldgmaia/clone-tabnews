46b7e1d8408900654a859bbd59893390850a6859
Diego Maia <ldgmaia@gmail.com>
2023-11-26 04:30:06 +0000

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div>
+            <p><b>Ligia,</b></p>
+            <p>So queria dizer que voce eh a melhor mae do mundo, uma esposa maravilhosa, uma amiga com quem sempre posso contar e alem de tudo isso, uma pessoa que faz o mundo ser um lugar melhor.</p>
+            <p>Sei que a situacao, neste momento, nao esta facil, mas tenho certeza que a partir de agora, tudo ira melhorar.</p>
+            <p>2024 vai ser melhor que 2023 e prometo fazer cada ano novo que vier melhor que o anterior.</p>
+            <p>Te amo muito, te amo pra sempre. Vou fazer tudo pra te fazer feliz todos os dias.</p>
+        </div>
+    )
+}
